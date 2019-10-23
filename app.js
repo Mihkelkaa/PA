75a123b843abad75c8b3eb51d7056a314d0a9945
Mihkelkaa <55825769+Mihkelkaa@users.noreply.github.com>
2019-10-23 16:18:06 +0300

    Andmetüübid

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("JS test");
+ // Andmetüübid
+
+ // täisarvud
+ console.log(10);
+ console.log(-4);
+ console.log(0);
+
+ // realarvud .ujuvkomaarvud
+
+ console.log(2.5);
+ console.log(-7.23);
+ console.log(-2.0);
+
+ // sõnad - tekst
+ console.log('Hello world!');
+
+ // tõeväärtused
+ console.log(true);
+ console.log(false);
+ 
+ // nimekirjad
+ console.log([1, 2, 3, 4, 5 ]);
+
+ //objektid
+console.log({a : 1, b : 2});
+console.table({a :1, b : 2});
+
