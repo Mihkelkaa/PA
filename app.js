a1045d959b87ce226f200e14d8d83895f003096f
Mihkelkaa <55825769+Mihkelkaa@users.noreply.github.com>
2019-10-28 11:49:22 +0200

    Algarvu leidmine

--- app.js
+++ app.js
@@ -1,53 +1,18 @@
 // Tsüklid
 
-// for Trükslid
-// üldtsüklid
-let arv;
-for(let arv = 1; arv <= 10; arv++){
-    lause ='arv = ';
-    if (arv < 10){
-    lause = lause + ' ';
-} 
-lause = lause + arv;
-if (arv % 2 == 0){
-    lause = lause + ' - paaris'
+// while 
+// eelkontrolliga tsükkel
+let arv = 5; // defineerime numbri
+// defineerime jagaja algväärtuse 
+let jagaja = 2
+//suurendame jagaja väärtust nii kaua, kui 
+//jagamise jääk on 0
+while(arv % jagaja !=0){
+    jagaja++;
+}
+// kontrollime , kas jagasime sama arvuga
+if(arv == jagaja){
+    console.log(arv + 'on algarv');
 } else {
-    lause = lause + ' - paaritu';
+    console.log(arv + ' ei ole algarv');
 }
-console.log(lause);
-
-if(arv >= 5) {
-    break;
-}
-}
-
-
-
-
-let numbrid = [];
-// lisame nimekirja sisse juhusliukud arvud
-for (let i = 0; i < 10; i++)
-{
-    let arv = Math.floor(Math.floor()* 10);
-    numbrid.push(arv);
-}
-// kontrollime tulemust
-console.log(numbrid);
-// sorteerime kasvujärjekorras
-//numbrid.sort();
-for (let i = 0; i < numbrid.length; i++){
-for (let kord = 0; kord < numbrid.length; kord++){
-    let abi;
-    if (numbrid [i] > numbrid [i+1]){
-        abi = numbrid [i];
-        numbrid [i] = numbrid [i+1];
-        numbrid [i+1] = abi;
-    }
-    console.log(numbrid);
-    
-}
-// kontrollime tulemust
-console.log(numbrid);
-}
-
-
